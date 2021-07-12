e165e2b9e4dd4d0402dbe41a84b422eecbc7a8c8
keerthi929 <69851175+keerthi929@users.noreply.github.com>
2021-07-12 19:20:18 +0530

    Add files via upload

---
+++ task23output.py
@@ -0,0 +1,13 @@
+{'name': 'John', 'age': 30, 'city': 'New York'}
+
+[10, 20, 30, 40, 55, 67, 89]
+
+(10, 20, 30, 40, 50)
+
+('Python', 'is', 'good ', 'programming', 'language')
+
+10
+
+6.3
+
+[Program finished]
